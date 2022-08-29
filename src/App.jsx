7bfa1d16e5f5c07c312f3e9a8507b045a16ca9aa
Mostafa Saeed <https://github.com/Desha-Saeed>
2022-08-29 08:37:24 +0200

    added route for production

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 import {
-  Navbar, Feed, SearchFeed, VideoDetails, ChannelDetails,
+  Navbar,
+  Feed,
+  SearchFeed,
+  VideoDetails,
+  ChannelDetails
 } from './components';
 
 function App() {
@@ -11,12 +15,30 @@ function App() {
       <Box sx={{ backgroundColor: '#000' }}>
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Feed />} />
-          <Route path="/video/:id" exact element={<VideoDetails />} />
-          <Route path="/channel/:id" exact element={<ChannelDetails />} />
-          <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+          <Route
+            path='/Youtube-Clone'
+            element={<Feed />}
+          />
+          <Route
+            path='/'
+            element={<Feed />}
+          />
+          <Route
+            path='/video/:id'
+            exact
+            element={<VideoDetails />}
+          />
+          <Route
+            path='/channel/:id'
+            exact
+            element={<ChannelDetails />}
+          />
+          <Route
+            path='/search/:searchTerm'
+            exact
+            element={<SearchFeed />}
+          />
         </Routes>
-
       </Box>
     </BrowserRouter>
   );
